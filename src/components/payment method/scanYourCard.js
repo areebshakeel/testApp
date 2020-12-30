8dba2ed27b44d68aa88951b096275fcec005e42e
areebshakeel <areebshakeel120@gmail.com>
2020-12-30 20:18:49 +0500

    Scan card screen done

--- src/components/payment method/scanYourCard.js
+++ src/components/payment method/scanYourCard.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, TextInput } from 'react-native'
 // import {Picker} from '@react-native-picker/picker '
 import { ScrollView } from 'react-native-gesture-handler'
 import CardImage from '../../../assets/add-card-img.png'
 import Button from '../Button'
 import Input from '../Input'
 import { Picker } from 'native-base'
-import { TextInput } from 'react-native-paper'
+// import { TextInput } from 'react-native-paper'
 
 
 export default function ScanYourCard() {
     const [selectedValue, setSelectedValue] = useState("MM");
-    const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    const years = [2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030]
+    const months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+    const years = ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030']
     return (
         <ScrollView style={{ backgroundColor: '#FFFF' }} >
             <View style={styles.container}>
@@ -39,46 +39,80 @@ export default function ScanYourCard() {
                 </View>
 
 
-                <View style={styles.pickerMainContainer}>
-                    <View>
-                    <Text style={{color:'#464951'}} >End Date</Text>
-                    <View style={styles.pickerContainer} >
+                {/* <View style={styles.pickerMainContainer}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '42%' }} >
+                        <View>
+                            <Text style={{ color: '#464951' }} >End Date</Text>
+                            <View style={styles.pickerContainer} >
 
-                        <Picker mode='dropdown'
-                            onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)} selectedValue={selectedValue} >
-                            {
-                                months.map((item, itemIndex) => {
-                                    <Picker.Item label={item} />
-                                })
-                            }
-                        </Picker>
+                                <Picker itemStyle={{color:'red',}} mode='dropdown'
+                                    onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)} 
+                                    selectedValue={'selectedValue'} >
+                                    {
+                                        months.map(item => <Picker.Item label={item} value={item} />)
+                                    }
+                                </Picker>
 
 
 
-                    </View>
-                    </View>
-                    <View>
-                        <Text>                </Text>
-                    <View style={styles.pickerContainer} >
-                        <Picker mode='dropdown'
-                            onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-                            selectedValue={selectedValue} >
-                            {
-                                years.map((item, itemIndex) => {
-                                    <Picker.Item label={item} />
-                                })
-                            }
+                            </View>
+                        </View>
+                        <View>
+                            <Text></Text>
+                            <View style={styles.picker2Container} >
+                            <Picker itemStyle={{color:'red',}} mode='dropdown'
+                                    onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)} 
+                                    selectedValue={'selectedValue'} >
+                                    {
+                                     years.map(item => <Picker.Item label={item} value={item} />)
+                                    }
+                                </Picker>
 
-                        </Picker>
-
-                    </View>
+                            </View>
+                        </View>
                     </View>
                     <View>
-                    <Text style={{color:'#464951'}} >CCV</Text>
+                        <Text style={{ color: '#464951' }} >CCV</Text>
 
-                    <View style={styles.ccvInput} >
-                                {/* <TextInput style={{height:'23%'}} /> */}
-                    </View >
+                        <View style={styles.ccvInput} >
+                            {/* <TextInput style={{minWidth:5, maxWidth:15}} /> */}
+                {/* </View >
+                    </View>
+                </View> */}
+
+                <View style={styles.pickerContainer} >
+                    <View style={{
+                        flexDirection: 'row', justifyContent: 'space-between',
+                        alignItems: 'center', width: '50%'
+                    }}>
+                        <View>
+                            <Text style={{color:'#464951'}}>End Date</Text>
+                            <View style={styles.picker1}>
+                                <Picker mode="dropdown">
+                                    {
+                                        months.map(item => <Picker.Item label={item} value={item} />)
+                                    }
+                                </Picker>
+                            </View>
+                        </View>
+                        <View>
+                            <Text></Text>
+                            <View style={styles.picker2}  >
+                                <Picker >
+                                    {
+                                        months.map(item => <Picker.Item label={item} value={item} />)
+
+                                    }
+                                </Picker>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View>
+                        <Text style={{color:'#464951'}} >CCV</Text>
+                        <View style={styles.ccvInput} >
+                            <TextInput style={{maxWidth:'40%', minWidth:'20%'}} />
+                        </View>
                     </View>
                 </View>
                 <View style={{ marginTop: 10 }}>
@@ -114,35 +148,54 @@ const styles = StyleSheet.create({
         fontFamily: 'Poppins-Bold',
         fontSize: 20
     },
-    pickerContainer: {
-        width: '100%',
-        // height: '100%',
-        backgroundColor: '#F8F8F8',
-        borderRadius: 8,
-        borderTopWidth: 1,
+    picker1: {
+        width: '40%',
+        paddingHorizontal: 20,
         borderBottomWidth: 1,
+        borderTopWidth: 1,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderRadius: 10,
+        backgroundColor: '#F8F8F8',
+        borderColor: '#707070',
+        alignItems: 'center',
+        alignSelf:'center',
+        paddingHorizontal:30
+
+    },
+    picker2: {
+        width: '40%',
+        paddingHorizontal: 20,
+        borderBottomWidth: 1,
+        borderTopWidth: 1,
         borderLeftWidth: 1,
         borderRightWidth: 1,
         borderColor: '#707070',
+        alignItems: 'center',
+        borderRadius: 10,
+        backgroundColor: '#F8F8F8',
+        alignSelf:'center',
+        paddingHorizontal:40
+
     },
-    pickerMainContainer: {
-        width: '95%',
+    pickerContainer: {
+        // backgroundColor:'red',
+        width: '90%',
         flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        marginTop: 20,
-        // backgroundColor:'green'
+        justifyContent: 'space-between'
     },
     ccvInput: {
-        width: '100%',
-        paddingHorizontal:40,
-        paddingVertical:24,
-        // height: '20%',
-        backgroundColor: '#F8F8F8',
-        borderRadius: 8,
-        borderTopWidth: 1,
         borderBottomWidth: 1,
+        borderTopWidth: 1,
         borderLeftWidth: 1,
         borderRightWidth: 1,
         borderColor: '#707070',
+        borderRadius: 10,
+        backgroundColor: '#F8F8F8',
+        paddingHorizontal:15,
+        
     }
+
 })
+
+
